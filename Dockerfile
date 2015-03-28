8c74a7deb56ed6dd297d60bdb7791ca5e484fba3
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2015-03-28 20:33:13 +0900

    Customize for higebu

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,10 @@ MAINTAINER  Yuya Kusakabe "yuya.kusakabe@gmail.com"
 # We use a bootstrap script to avoid having temporary cache files and build
 # dependencies being committed and included into the docker image.
 ADD         bootstrap.sh /tmp/
-RUN         chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
+RUN         chmod +x /tmp/bootstrap.sh; sync; /tmp/bootstrap.sh
 
 RUN         useradd znc
 ADD         start-znc /usr/local/bin/
-ADD         znc.conf.default /src/
-RUN         chmod 644 /src/znc.conf.default
 
 EXPOSE      6667
 ENTRYPOINT  ["/usr/local/bin/start-znc"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
-
 # Config
-ZNC_VERSION="1.6"
-
+ZNC_VERSION="1.6.0"
 
 # Ensure package list is up to date.
 apt-get update
@@ -15,10 +13,8 @@ apt-get install -y sudo
 # Install build dependencies.
 apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config
 
-
 # Prepare building
 mkdir -p /src
-
 
 # Download, compile and install ZNC.
 cd /src
@@ -27,8 +23,5 @@ tar -zxf "znc-${ZNC_VERSION}.tar.gz"
 cd "znc-${ZNC_VERSION}"
 ./configure && make && make install
 
-
 # Clean up
-apt-get remove -y wget
-apt-get autoremove -y
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
